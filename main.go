9265ee6dfd3f7046b2b0c7cc547480b2707296fa
Ted Pearson <ted.pearson@oracle.com>
2021-12-11 00:53:50 -0500

    try a 10 sec pause

--- main.go
+++ main.go
@@ -148,7 +148,7 @@ func (app App) DeleteSeries(host string, measurements ...string) {
 	if err != nil {
 		panic(errors.Wrap(err, "Failed to delete series from victoriametrics."))
 	}
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 10)
 }
 
 func (app App) RunAstrocast(forecaster weather.Forecaster, options weather.WriteOptions) {
