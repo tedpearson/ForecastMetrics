ed788e21e2d2eac70ac3b2155d339cfee9147d02
Ted Pearson <ted.pearson@oracle.com>
2022-01-30 00:38:40 -0500

    attempt to build a debian package

---
+++ packaging/deb.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+dir=$(dirname "${BASH_SOURCE[0]}")
+
+BINARY=$1; shift
+VERSION=$1; shift
+DEB_FILE=$1; shift
+
+mkdir -p deb/usr/local/bin
+cp $BINARY deb/usr/local/bin/
+mkdir -p deb/usr/local/share/forecastmetrics
+cp "$dir/../config/forecastmetrics.example.yaml" deb/usr/local/share/forecastmetrics/
+mkdir -p package/DEBIAN
+cat > package/DEBIAN/control <<-END
+	Package: forecastmetrics
+	Version: $VERSION
+	Architecture: armhf
+	Maintainer: Ted Pearson <ted@tedpearson.com>
+	Description: Ingests forecast data into time-series database.
+END
+
+dpkg-deb --build --root-owner-group package "$DEB_FILE"
